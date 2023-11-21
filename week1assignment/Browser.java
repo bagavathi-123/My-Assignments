cf8458538d7ac2a08a106aefcf77dab4cf76781f
bagavathi-123 <150062400+bagavathi-123@users.noreply.github.com>
2023-11-21 23:56:49 +0530

    week1assignment

---
+++ week1assignment/Browser.java
@@ -0,0 +1,20 @@
+package week1assignment;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		float browserversion=100.2f;
+		String browsername="firefox";
+		int releaseyear=1998;
+		boolean isVisible=true;
+		char browserlogo='f';
+		System.out.println("browser version:" +browserversion);
+		System.out.println("browser name:"+browsername);
+		System.out.println("boolean is visible:"+isVisible);
+		System.out.println("browser logo:"+browserlogo);
+		System.out.println("release year:"+releaseyear);
+		
+
+	}
+
+}
