08a9cfe243dbcafc709b3492b0a5278b87cc9084
bagavathi-123 <150062400+bagavathi-123@users.noreply.github.com>
2023-11-21 23:55:56 +0530

    week1day1

---
+++ week1/day1/Findnumber.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Findnumber {
+
+	public static void main(String[] args) {
+		int num=45;
+		if (num>0) {
+			System.out.println("positive number :"+num);
+		}else {
+			System.out.println("negative number :"+num);
+		}
+
+	}
+
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserversion=100.2f;
+		String browsername="firefox";
+		int releaseyear=1998;
+		boolean pageloaded=true;
+		char browserlogo='f';
+		System.out.println("browser version:" +browserversion);
+		System.out.println("browser name:"+browsername);
+		System.out.println("boolean page loaded :"+pageloaded);
+		System.out.println("browser logo:"+browserlogo);
+		System.out.println("release year:"+releaseyear);
+		
+
+	}
+
+}
